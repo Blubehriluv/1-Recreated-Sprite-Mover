2e55e9a719b41bc92dfeb0f47cf4e772c3fce0c7
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-09-13 16:16:29 -0700

    Having Issues with Buttons
    can't get the darn script to show up for the button press

--- Sprite Mover/Assets/Scripts/StartButton.cs
+++ Sprite Mover/Assets/Scripts/StartButton.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
+    Canvas mainMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void changeScreen()
+    {
+        Debug.Log("Hi there");
     }
 }
